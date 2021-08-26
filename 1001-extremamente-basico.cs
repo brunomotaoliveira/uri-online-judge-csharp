57e50878eb667b9ee3efea6b7d8471f7a437ae32
brunomotadev <brunomotadev@gmail.com>
2021-08-26 15:37:51 -0300

    1001 - extremamente basico

---
+++ 1001-extremamente-basico.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ExtremamenteBasico
+{
+    class URI2
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int x = a + b;
+
+            Console.WriteLine($"X = {x}");
+        }
+    }
+}
