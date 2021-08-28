c0f85586498d04973a8f20220224e130268ec975
brunomotadev <brunomotadev@gmail.com>
2021-08-28 18:38:17 -0300

    Diferenca entre 4 numeros

---
+++ 1007-diferenca.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Diferenca 
+{
+    class UriDiferenca
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int d = Convert.ToInt32(Console.ReadLine());
+
+            int diferenca = ((a * b) - (c * d));
+
+            Console.WriteLine($"DIFERENCA = {diferenca}");
+
+        }
+    }
+}
