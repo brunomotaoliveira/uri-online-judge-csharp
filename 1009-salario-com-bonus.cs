bc45efe38a9f690763b58d2fe17f808bf6a82927
brunomotadev <brunomotadev@gmail.com>
2021-08-28 19:03:51 -0300

    Salario com Bonus

---
+++ 1009-salario-com-bonus.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace SalarioComBonus
+    {
+        class UriSalarioComBonus
+            {
+                static void Main(string[] args)
+                {
+                    string nomeDoVendedor = Convert.ToString(Console.ReadLine());
+                    double salarioFixo = Convert.ToDouble(Console.ReadLine());
+                    double totalDeVendas = Convert.ToDouble(Console.ReadLine());
+
+                    double totalParaReceber = salarioFixo + ((totalDeVendas * 15)/100);
+
+                    Console.WriteLine($"TOTAL = R$ {totalParaReceber:f2}");
+                }
+            }
+    }
